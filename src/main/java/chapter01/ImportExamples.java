e6bf471c17afd6dd4f07302f620db0d9a2652d7f
vshemyako <31586908+vshemyako@users.noreply.github.com>
2017-10-12 14:24:54 +0300

    Revert "Revert "Feature/learn java building blocks""

---
+++ src/main/java/chapter01/ImportExamples.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+import java.util.*;
+import java.sql.*;
+
+public class ImportExamples {
+    public static void main(String[] args) {
+        System.out.println("Import example");
+    }
+}
